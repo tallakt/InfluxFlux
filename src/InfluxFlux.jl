0447af89c627157864a456e1069118232267f4c3
Tallak Tveide <tt@kitemill.no>
2023-12-14 16:59:14 +0100

    Use tabs 4 spaces

--- src/InfluxFlux.jl
+++ src/InfluxFlux.jl
@@ -14,99 +14,99 @@ TimeSpec = Union{Int,DateTime,ZonedDateTime}
 
 
 struct InfluxServer
-  uri::String
-  org::String
-  api_token::String
+    uri::String
+    org::String
+    api_token::String
 end
 
 
 function time_spec_to_epoc_ns(time_spec::Int)
-  time_spec
+    time_spec
 end
 
 
 function time_spec_to_epoc_ns(time_spec::ZonedDateTime)
-  time_spec_to_epoc_ns(DateTime(time_spec, UTC))
+    time_spec_to_epoc_ns(DateTime(time_spec, UTC))
 end
 
 
 function time_spec_to_epoc_ns(time_spec::DateTime)
-  Int(1_000_000_000 * datetime2unix(time_spec))
+    Int(1_000_000_000 * datetime2unix(time_spec))
 end
 
 
 function uri_helper(srv::InfluxServer, path::String)
-  HTTP.URI(HTTP.URI("$(srv.uri)/$path"), query = Dict("org" => srv.org))
+    HTTP.URI(HTTP.URI("$(srv.uri)/$path"), query = Dict("org" => srv.org))
 end
 
 
 function token_json_headers(srv::InfluxServer)
-  Dict("Authorization" => "Token $(srv.api_token)"
-       , "Accept" => "application/json")
+    Dict("Authorization" => "Token $(srv.api_token)"
+         , "Accept" => "application/json")
 end
 
 
 function influx_server(uri::String, org::String, api_token::String)::InfluxServer
-  InfluxServer(uri, org, api_token)
+    InfluxServer(uri, org, api_token)
 end
 
 
 function flux(srv::InfluxServer, flux_query::String)
-  headers = merge(token_json_headers(srv), Dict("Content-Type" => "application/vnd.flux"))
-  response = HTTP.post(uri_helper(srv, "api/v2/query"), headers, flux_query);
-  if response.status == 200
-    response.body
-  else
-    throw(response.status)
-  end
+    headers = merge(token_json_headers(srv), Dict("Content-Type" => "application/vnd.flux"))
+    response = HTTP.post(uri_helper(srv, "api/v2/query"), headers, flux_query);
+    if response.status == 200
+        response.body
+    else
+        throw(response.status)
+    end
 end
 
 
 function flux_to_dataframe(srv::InfluxServer, flux_query::String)
-  CSV.File(flux(srv, flux_query), delim = ',') |> DataFrame
+    CSV.File(flux(srv, flux_query), delim = ',') |> DataFrame
 end
 
 
 function measurement(srv::InfluxServer, bucket::String, measurement_name::String, from::TimeSpec, to::TimeSpec)
-  q = """
+    q = """
     from(bucket: "$bucket")
     |> range(start: time(v: uint(v: $(time_spec_to_epoc_ns(from)))), stop: time(v: uint(v: $(time_spec_to_epoc_ns(to)))))
-      |> filter(fn: (r) => r._measurement == "$measurement_name")
-      |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-      |> map(fn: (r) => ({ r with _time: uint(v: r._time) }))
-  """
-  #println(q)
-  result = flux_to_dataframe(srv, q)
-  result[:,7:end] # remove influxdb internal columns
+    |> filter(fn: (r) => r._measurement == "$measurement_name")
+    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+    |> map(fn: (r) => ({ r with _time: uint(v: r._time) }))
+    """
+    #println(q)
+    result = flux_to_dataframe(srv, q)
+    result[:,7:end] # remove influxdb internal columns
 end
 
 
 function aggregate_measurement(srv::InfluxServer, bucket::String, measurement_name::String, from::TimeSpec, to::TimeSpec, window::Period; fn::String = "mean")
-  q = """
+    q = """
     from(bucket: "$bucket")
     |> range(start: time(v: uint(v: $(time_spec_to_epoc_ns(from)))), stop: time(v: uint(v: $(time_spec_to_epoc_ns(to)))))
-      |> filter(fn: (r) => r._measurement == "$measurement_name")
-      |> aggregateWindow(every: $(Nanosecond(window).value)ns, fn: $fn, createEmpty: false)
-      |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-      |> map(fn: (r) => ({ r with _time: uint(v: r._time) }))
-  """
-  #println(q)
-  result = flux_to_dataframe(srv, q)
-  result[:,7:end] # remove influxdb internal columns
+    |> filter(fn: (r) => r._measurement == "$measurement_name")
+    |> aggregateWindow(every: $(Nanosecond(window).value)ns, fn: $fn, createEmpty: false)
+    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+    |> map(fn: (r) => ({ r with _time: uint(v: r._time) }))
+    """
+    #println(q)
+    result = flux_to_dataframe(srv, q)
+    result[:,7:end] # remove influxdb internal columns
 end
 
 
 function measurements(srv::InfluxServer, bucket::String)
-  q = """
+    q = """
     import "influxdata/influxdb/schema"
     schema.measurements(bucket: "$bucket")
-  """
-  String.(InfluxFlux.flux_to_dataframe(srv, q)[:, "_value"])
+    """
+    String.(InfluxFlux.flux_to_dataframe(srv, q)[:, "_value"])
 end
 
 
 function buckets(srv::InfluxServer)
-  String.(InfluxFlux.flux_to_dataframe(srv, "buckets()")[:, :name])
+    String.(InfluxFlux.flux_to_dataframe(srv, "buckets()")[:, :name])
 end
 
 end # module
